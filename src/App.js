291f1c9bdaa70126bb6d24d0c8dbbbd453503649
Natural <jarkurghan@gmail.com>
2024-02-18 17:30:56 +0500

    test commit #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import HomePage from "./pages/Home";
+import SearchPage from "./pages/Search";
+import WordPage from "./pages/Word";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="app">
+            <Header />
+            <ToastContainer />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/word/:word" element={<WordPage />} />
+                <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Word.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Word from "../components/Word/Word";
+
+const WordPage = () => {
+    return (
+        <div>
+            <Word />
+        </div>
+    );
+};
+
+export default WordPage;
