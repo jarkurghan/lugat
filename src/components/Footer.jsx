d39907e81b006f282763bef8ab53463cd9685f12
Natural <jarkurghan@gmail.com>
2024-03-16 18:33:12 +0500

    new commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <div className="h-full flex justify-center items-center">
-                {/* <div>footer</div> */}
-            </div>
+            <div className="h-full flex justify-center items-center">{/* <div>footer</div> */}</div>
         </div>
     );
 }
