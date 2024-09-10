a41e3912688caa2d68bfc755160219bc48584c6c
Natural <jarkurghan@gmail.com>
2024-09-10 22:15:37 +0500

    update title

--- src/components/Search/Result.jsx
+++ src/components/Search/Result.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { getResult, getWord } from "../../store/word";
 import { useSelector } from "react-redux";
@@ -9,12 +9,18 @@ import { getWait } from "../../store/waiting";
 function Search() {
     const results = useSelector(getResult);
     const spinner = useSelector(getWait);
+    const word = useSelector(getWord);
+
+    const [req, setReq] = useState(word);
+    useEffect(() => {
+        setReq(word)
+    }, [results])
 
     return (
         <div className="max-w-5xl xl:max-w-4xl m-6 xl:ml-40">
             <Navbar />
             <div>
-                {results.length > 0 && <div className="m-4 lowercase text-gray-500 text-[14px]">qidiruv natijalari</div>}
+                {results.length > 0 && <div className="m-4 lowercase text-gray-500 text-[14px]">"{req}" bo'yicha qidiruv natijalari</div>}
                 {results.map((e) => (
                     <SearchItem key={e.word} data={e} />
                 ))}
